fc650806cf9a00c14103d04b24372ad75029115f
Salsa-socks <dbongs@gmail.com>
2023-01-14 14:34:20 +0200

    Set up done, navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/Navbar';
+// components arranged from top to bottom of screen
+/* navbar, slideshow component import*/
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-//logo changed in home logo.svg - kept logo place, changed svg after redrawing logo
 import logo from '../logo.svg';
 import './Navbarstyle.css';
-//React router for page nav
-import { BrowserRouter, Route, Link } from "react-router-dom";
+
+//This should be function Navbar() for routing -> layout.js -> app.js (browser route and route paths) -> Index.js
+//didnt do routing cause all pages dont need to be done
 
 class Navbar extends Component {
     // click handle for responsive navbar - menu slides in from right when clicking burger icon
-    // upon clicking close icon reverse happens. State change on line 29 if bars(burger icon) clicked then show X and vice-versa 
+    // upon clicking close icon reverse happens. State change on line 29 if bars(burger icon) clicked then show X and vice-versa
+    // state and handle click used cause not a routing func
     state = {clicked : false};
     handleClick = () => {
         this.setState({clicked : !this.state.clicked})
     }
-    render() {     
+    render() {
+        
         return (
             <div>
                 <nav>
@@ -21,19 +23,16 @@ class Navbar extends Component {
                     </a>
                     <div>
                         <ul id='links' className = {this.state.clicked ? "#links active"  : "links"}>
-                            <li>
-                                <a className='active' href='../public/index.html' ></a>cases
-                            </li>
+                            <li><a className='active' href='../public/index.html' ></a>cases</li>
                             <li><a href='../public/index.html'></a>career</li>
                             <li><a href='../public/index.html'></a>contact</li>
                             <li><a href='../public/index.html'></a>culture</li>
                         </ul>
+
                     </div>
                     <div id='mobile' onClick = {this.handleClick}>
                         <i id='bar' className = {this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                     </div>
-                   
-
                 </nav>
             </div>
         );
