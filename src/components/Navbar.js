88083d5f117ff819c0bf1d25f29f6004b0be6204
Salsa-socks <dbongs@gmail.com>
2023-01-14 12:25:46 +0200

    added dependency: react router, react grid

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './Navbarstyle.css';
+
+class Navbar extends Component {
+        // click handle for responsive navbar - menu slides in from right when clicking burger icon
+    // upon clicking close icon reverse happens. State change on line 29 if bars(burger icon) clicked then show X and vice-versa
+    state = {clicked : false};
+    handleClick = () => {
+        this.setState({clicked : !this.state.clicked})
+    }
+    render() {
+        
+        return (
+            <div>
+                <nav>
+                    <a href='../public/index.html' >
+                    <img src={logo} className="nav-logo" alt="logo" />
+                    </a>
+                    <div>
+                        <ul id='links' className = {this.state.clicked ? "#links active"  : "links"}>
+                            <li><a className='active' href='../public/index.html' ></a>cases</li>
+                            <li><a href='../public/index.html'></a>career</li>
+                            <li><a href='../public/index.html'></a>contact</li>
+                            <li><a href='../public/index.html'></a>culture</li>
+                        </ul>
+                    </div>
+                    <div id='mobile' onClick = {this.handleClick}>
+                        <i id='bar' className = {this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+                   
+
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
